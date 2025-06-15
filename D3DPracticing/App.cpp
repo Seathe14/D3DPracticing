7689035ff394fa2f7d9fc4fd79f778e3eec06bf2
Seathe14 <45715728+Seathe14@users.noreply.github.com>
2025-06-15 20:52:32 +0300

    Add Pyramid drawable

--- D3DPracticing/App.cpp
+++ D3DPracticing/App.cpp
@@ -3,6 +3,7 @@
 #include <random>
 
 #include "Box.h"
+#include "Pyramid.h"
 #include "Graphics.h"
 
 App::App() : wnd(800,600, "myWnd")
@@ -12,13 +13,17 @@ App::App() : wnd(800,600, "myWnd")
 	std::uniform_real_distribution<float> ddist(0.0f, 3.1415f * 2.0f);
 	std::uniform_real_distribution<float> odist(0.0f, 3.1415f * 0.3f);
 	std::uniform_real_distribution<float> rdist(6.0f, 15.0f);
-	for (auto i = 0; i < 120; i++)
-	{
-		boxes.push_back(std::make_unique<Box>(
-			wnd.GetGfx(), rng, adist,
-			ddist, odist, rdist
-		));
-	}
+        for (auto i = 0; i < 60; i++)
+        {
+                drawables.push_back(std::make_unique<Box>(
+                        wnd.GetGfx(), rng, adist,
+                        ddist, odist, rdist
+                ));
+                drawables.push_back(std::make_unique<Pyramid>(
+                        wnd.GetGfx(), rng, adist,
+                        ddist, odist, rdist
+                ));
+        }
 	wnd.GetGfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 40.0f));
 }
 
@@ -47,11 +52,11 @@ void App::DoFrame()
 	auto dt = tmr.Mark();
 	wnd.GetGfx().ClearBuffer(0.07f, 0.0f, 0.12f);
 
-	for (auto& b : boxes)
-	{
-		b->Update(dt);
-		b->Draw(wnd.GetGfx());
-	}
+        for (auto& d : drawables)
+        {
+                d->Update(dt);
+                d->Draw(wnd.GetGfx());
+        }
 
 	wnd.GetGfx().EndFrame();
 }
